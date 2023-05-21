d5612e1078b320cef13d75cf16aa507e0f348397
ritesh-25 <100701018+ritesh-25@users.noreply.github.com>
2023-05-21 10:17:37 +0530

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+# GithubFinder
+Github finder is a Fetch Api project where we can easily compare github user's information by simply typing their username. This application shows user profile, Public repositories, Followers, Following.
---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+// Search Input
+// const searchUser = document.getElementById('searchUser');
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+const item = [];
+const item2=[];
+app.get("/",function(req,res){
+
+res.render("index",{newListItems: item});
+
+});
+//Initialize GitHub
+class GitHub{
+    constructor(){
+        this.client_id = "b538be69eaef6f6b6559";
+        this.client_secret = "0f478dbe8a73c409c92c44a5d9aae98e7f4efd8d";
+        this.repos_count = 5;
+        this.repos_sort = 'created: asc'
+    }
+    async getUser(user){
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+        const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`)
+        const profile = await profileResponse.json();
+        const repos = await repoResponse.json();
+
+        return {
+            profile,
+            repos
+        }
+    }
+}
+const github = new GitHub;
+app.post("/",function(req,res){
+const userText = req.body.username;
+if(item2.includes(userText)===false){
+item2.push(userText);
+if(userText !== ''){
+            // Make http call
+            github.getUser(userText)
+            .then(data => {
+                if(data.profile.message !== "Not Found"){
+                   item.push(data.profile);
+                   console.log(data.profile);
+                }
+            });
+            // console.log(userText);
+}
+}
+// console.log(item.length);
+res.redirect("/");
+});
+
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
+  
